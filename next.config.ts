4f63a353a9fb5580dc3f17ab639c7c1e11b8abf8
ankitoid <ankitsinghghoshi1234@gmail.com>
2025-12-29 16:46:04 +0530

    error  commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.drydash.in", // incoming domain
+          },
+        ],
+        destination: "https://drydash.in/:path*", // target domain
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
